151b559318f66ed481854c80cc6d776353ef3dd0
Luis Monsalve <luismonsalve1941@gmail.com>
2023-05-17 23:32:12 -0400

    :sparkles: feat: update the project script and github templates

--- TODO.md
+++ TODO.md
@@ -1,1 +1,10 @@
 # Todo
+
+[ ] update the packages
+[ ] better readme
+[ ] update the package.json
+
+[ ] change the background color
+
+[ ] add all the icons fir files
+[ ] change the color of the folders
--- src/icon/icon_name.ts
+++ src/icon/icon_name.ts
@@ -16,6 +16,22 @@ const iconName =
     name: 'typescript',
     languageIds: [ 'ts' ],
     fileExtensions: [ 'ts' ]
+  },
+  {
+    name: 'pnpm',
+    fileNames: ['pnpm-lock.yaml', 'pnpm-workspace.yaml', '.pnpmfile.cjs']
+  },
+  { name: 'nodemon', fileNames: ['nodemon.json', 'nodemon-debug.json'] },
+  {
+    name: 'husky',
+    fileNames: [
+      '.huskyrc',
+      'husky.config.js',
+      '.huskyrc.json',
+      '.huskyrc.js',
+      '.huskyrc.yaml',
+      '.huskyrc.yml'
+    ]
   }
 ];
 
